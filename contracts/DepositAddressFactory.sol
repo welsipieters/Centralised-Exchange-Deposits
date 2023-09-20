e64fff49b38e7dce0b194b10148ec604ddafbd70
Wesley Peeters <wesley@pixieinteractive.io>
2023-09-20 21:38:55 +0200

    2nd pass on depositContract

--- contracts/DepositAddressFactory.sol
+++ contracts/DepositAddressFactory.sol
@@ -1,28 +1,66 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract DepositAddressFactory {
-    address public owner;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract DepositContract is AccessControl, ReentrancyGuard, Pausable {
+    bytes32 public constant OWNER_ROLE = keccak256("OWNER_ROLE");
     address public coldStorage;
-    DepositContract[] public contracts;
+
+    // Events
+    event EtherSwept(address indexed to, uint256 amount);
+    event ERC20TokenSwept(address indexed tokenAddress, address indexed to, uint256 amount);
+    event ERC721TokenSwept(address indexed nftAddress, uint256 tokenId, address indexed to);
 
     constructor(address _coldStorage) {
-        owner = msg.sender;
+        _setupRole(OWNER_ROLE, msg.sender);
         coldStorage = _coldStorage;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
+        require(hasRole(OWNER_ROLE, msg.sender), "Not authorized");
         _;
     }
 
-    function deployNewContract() external onlyOwner returns(address) {
-        DepositContract newContract = new DepositContract(coldStorage);
-        contracts.push(newContract);
-        return address(newContract);
+    function pause() external onlyOwner {
+        _pause();
     }
 
-    function getDeployedContracts() external view returns (DepositContract[] memory) {
-        return contracts;
+    function unpause() external onlyOwner {
+        _unpause();
     }
+
+    function sweepERC20Token(address tokenAddress) external onlyOwner whenNotPaused nonReentrant {
+        IERC20 token = IERC20(tokenAddress);
+        uint256 tokenBalance = token.balanceOf(address(this));
+        if (tokenBalance > 0) {
+            require(token.transfer(coldStorage, tokenBalance), "Token transfer failed");
+            emit ERC20TokenSwept(tokenAddress, coldStorage, tokenBalance);
+        }
+    }
+
+    function sweepERC721Tokens(address nftAddress, uint256[] memory tokenIds) external onlyOwner whenNotPaused nonReentrant {
+        IERC721 nft = IERC721(nftAddress);
+        for (uint256 i = 0; i < tokenIds.length; i++) {
+            uint256 tokenId = tokenIds[i];
+            require(nft.ownerOf(tokenId) == address(this), "Token not owned by contract");
+            nft.transferFrom(address(this), coldStorage, tokenId);
+            emit ERC721TokenSwept(nftAddress, tokenId, coldStorage);
+        }
+    }
+
+    function sweepEther() external onlyOwner whenNotPaused nonReentrant {
+        uint256 ethBalance = address(this).balance;
+        if (ethBalance > 0) {
+            payable(coldStorage).transfer(ethBalance);
+            emit EtherSwept(coldStorage, ethBalance);
+        }
+    }
+
+    // Allow the contract to accept ether
+    receive() external payable {}
 }
