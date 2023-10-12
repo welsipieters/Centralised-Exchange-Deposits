6152af3491302e3a2a86ac046a496b84d4b009be
Wesley Peeters <wesley@pixieinteractive.io>
2023-10-12 21:01:22 +0200

    Move config to .env

--- api/index.ts
+++ api/index.ts
@@ -14,43 +14,31 @@ import axios from "axios";
 import postSweepTransactions from "./crons/postSweepTransaction";
 import fetchAddressesFromExternalAPI from "./crons/fetchAddressesFromExternalAPI";
 
-
-
-
-
 const config = {
-    knakenURL: 'https://api.dev.knaken.eu/api/',
+    knakenURL: process.env.KNAKEN_URL!,
     keys: {
-        core: 'zq9i6UhkEz39oXJNjnua6kNPqUJkqbeVYmF69AFVNz3b97dbtuJsBCY27228LP7N',
-        admin: 'rOd51HU38leItTXBxL2Jvc8bO8V64vfxDBLQeieUff5ONwxUGYM9tpRvkUP9AX9D',
+        core: process.env.CORE_KEY!,
+        admin: process.env.ADMIN_KEY!,
     }
 };
 
-
-
-
 cron.schedule('*/5 * * * *', () => postSweepTransactions(config));
-
-
-
-
-
-cron.schedule('*/1 * * * *', () => fetchAddressesFromExternalAPI(config));  // Schedule the task to run every 1 minute
+cron.schedule('*/1 * * * *', () => fetchAddressesFromExternalAPI(config));
 
 
 const main = async () => {
     await initializeDatabase();
+
     fetchAddressesFromExternalAPI(config)
     postSweepTransactions(config);
-    const app = express();
 
+    const app = express();
     app.use('/deposit-addresses', DepositAddressRoutes(container));
 
     const port = process.env.APP_PORT || 3000;
 
     app.listen(port, async () => {
         console.log(`Server started on port ${port}`);
-
     });
 };
 
